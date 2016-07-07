7e72a44f45a11b3a9961f01eae7c29dc1051ce13
Priya <priya.pancholi@promactinfo.com>
2016-07-07 10:35:45 +0530

    router config exercise

--- MVCWithEF/App_Start/RouteConfig.cs
+++ MVCWithEF/App_Start/RouteConfig.cs
@@ -13,11 +13,21 @@ namespace MVCWithEF
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+           
+
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+               name: "homepage",
+               url: "product",
+               defaults: new { controller = "MvcEF", action = "ProductInfo", id = UrlParameter.Optional }
+           );
+
+
+            routes.MapRoute(
+               name: "Default",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+           );
         }
     }
 }
---
+++ MVCWithEF/Controllers/MvcEFController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWithEF.Controllers
+{
+    public class MvcEFController : Controller
+    {
+        // GET: MvcEF
+        public ActionResult ProductInfo()
+        {
+            return View();
+        }
+    }
+}
