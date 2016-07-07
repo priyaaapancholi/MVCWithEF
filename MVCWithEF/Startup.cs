54a58ac53012db9109ea7e6c2e047e993789a2c9
Priya <priya.pancholi@promactinfo.com>
2016-07-07 10:08:01 +0530

    Add project files.

---
+++ MVCWithEF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCWithEF.Startup))]
+namespace MVCWithEF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
